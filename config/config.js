aeb15c9603a0e5bd6cc9309c5d47d5de4457951e
Carlos Henrique <ica015@hotmail.com>
2023-09-29 23:38:48 -0300

    Inicio do projeto

---
+++ config/config.js
@@ -0,0 +1,10 @@
+module.export = {
+        username: "cond",
+        password: 'cond1234',
+        database: "condminio",
+        host: "localhost",
+        dialect: "mysql",
+        define:{
+            timestamp: true
+        }
+}
